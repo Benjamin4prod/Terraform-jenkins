37bf597e1ada8e7b56c924230331dcc7386688a0
Benjamin4prod <136221097+Benjamin4prod@users.noreply.github.com>
2023-06-15 21:32:22 -0500

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
-sudo yum update -y
 
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 
-sudo amazon-linux-extras install java-openjdk11 -y && sudo yum install jenkins -y
-sudo systemctl daemon-reload
+sudo apt-get update
+sudo apt-get zip unzip
 
+
+#install git
+
+sudo apt-get install git
+
+#install jenkins
+
+sudo apt update
+sudo apt install -y openjdk-11-jre 
+
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
+
+
+#install git
+
+sudo apt-get install git -y
+
+#install terraform
+
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform -y
+
+#install aws cli to validate terraform
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
