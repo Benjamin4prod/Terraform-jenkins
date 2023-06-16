2e86ff8681a2201f7d9bd456b9bc7d851bc52b0a
Benjamin4prod <136221097+Benjamin4prod@users.noreply.github.com>
2023-06-15 21:45:51 -0500

    Update jenkins_bootstrap.sh

--- jenkins_bootstrap.sh
+++ jenkins_bootstrap.sh
@@ -32,9 +32,9 @@ sudo yum install git -y
 
 #install terraform
 
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo yum update && sudo yum install terraform -y
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
 
 #install aws cli
 
