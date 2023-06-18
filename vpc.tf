28564f5f963bd376fe5f2bbc315ea4ad3502e73c
Benjamin4prod <136221097+Benjamin4prod@users.noreply.github.com>
2023-06-18 01:17:54 +0000

    changes

--- vpc.tf
+++ vpc.tf
@@ -1,47 +1,46 @@
-#retrieve azs
-
+# Retrieve AZs
 data "aws_availability_zones" "available" {
-  state           = "available"  
+  state = "available"
 }
 
 data "aws_region" "current" {}
 
 resource "aws_vpc" "vpc" {
-  cidr_block = var.vpc_cidr
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = var.enable_dns_hostnames
 
   tags = {
-    Name = "${var.vpc_name}-vpc"
-    environment = "var.environment"
-    Terraform = "true"
+    Name        = "${var.vpc_name}-vpc"
+    environment = var.environment
+    Terraform   = "true"
   }
 }
 
 resource "aws_subnet" "public_subnets" {
-  vpc_id = aws_vpc.vpc.id
-  count = len(var.public_subnets)
-  cidr_block = cidrsubnet(var.vpc_cidr, 8, each.value+15)
+  vpc_id            = aws_vpc.vpc.id
+  count             = length(var.public_subnets)
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value + 15)
   availability_zone = data.aws_availability_zones.available.names[0]
 
   map_public_ip_on_launch = true
 
   tags = {
-    name = "${var.vpc_name}_${count.index}"
+    name        = "${var.vpc_name}_${count.index}"
     environment = var.environment
-    Terraform = true
+    Terraform   = true
   }
 }
 
 resource "aws_subnet" "private_subnets" {
-  vpc_id = aws_vpc.vpc.id
-  count = len(var.private_subnets)
-  cidr_block = cidrsubnet(var.vpc_cidr, 8, each.value+15)
+  vpc_id            = aws_vpc.vpc.id
+  count             = length(var.private_subnets)
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value + 15)
   availability_zone = data.aws_availability_zones.available.names[1]
 
   tags = {
-    name = "${var.vpc_name}_${count.index}"
+    name        = "${var.vpc_name}_${count.index}"
     environment = var.environment
-    Terraform = true
+    Terraform   = true
   }
 }
 
@@ -49,9 +48,9 @@ resource "aws_internet_gateway" "IGW" {
   vpc_id = aws_vpc.vpc.id
 
   tags = {
-    name = "${var.vpc_name}_igw"
+    name        = "${var.vpc_name}_igw"
     environment = var.environment
-    Terraform = "true"
+    Terraform   = "true"
   }
 }
 
@@ -63,16 +62,15 @@ resource "aws_default_route_table" "public_route_table" {
   }
 
   tags = {
-    name = "${var.vpc_name}_public_rt"
+    name        = "${var.vpc_name}_public_rt"
     environment = var.environment
-    Terraform = "True"
+    Terraform   = "True"
   }
 }
 
 resource "aws_route_table_association" "route_table_attachment" {
-depends_on = [ aws_subnet.public_subnets ]
-route_table_id = aws_default_route_table.public_route_table.id
-for_each = aws_subnet.public_subnets
-subnet_id = each.value.id
+  depends_on      = [aws_subnet.public_subnets]
+  route_table_id  = aws_default_route_table.public_route_table.id
+  for_each        = aws_subnet.public_subnets
+  subnet_id       = each.value.id
 }
-
